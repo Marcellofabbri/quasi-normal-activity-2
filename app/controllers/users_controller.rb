e76ab521c5ac29f216d980425af70ed47198deb4
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-11-22 00:45:04 +0000

    add stories to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,21 @@ class UsersController < ApplicationController
     @user = User.find_by(username: current_user.username)
     @categories = @user.categories
     @handle = handle(@user)
-    @story = Story.new
+    @stories = stories
   end
 
   def handle(user)
     user.username + "#{user.id}"
   end
+
+  def stories
+    allStories = []
+    @categories.each do |category|
+      category.stories.each do |story|
+        allStories.push(story)
+      end
+    end
+    return allStories
+  end
+
 end
